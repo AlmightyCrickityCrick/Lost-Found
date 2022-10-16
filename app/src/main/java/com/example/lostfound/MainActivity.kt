824ba8545a6701f2f16ebd41e21cf4e451fd58a9
AlmightyCrickityCrick <seong92@mail.ru>
2022-10-17 00:08:08 +0300

    Login UI Design scheme

--- app/src/main/java/com/example/lostfound/MainActivity.kt
+++ app/src/main/java/com/example/lostfound/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.lostfound
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lostfound.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var login = Intent(this, LoginActivity::class.java)
+        startActivity(login)
     }
 }
